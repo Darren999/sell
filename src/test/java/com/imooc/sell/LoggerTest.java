08cfaaf516feaff011b62b2a70a25218f4c56e75
darrren.jian <darrren@darrren-Mac.local>
2019-04-28 20:11:06 +0800

    create project,add Slf4j

---
+++ src/test/java/com/imooc/sell/LoggerTest.java
@@ -0,0 +1,20 @@
+package com.imooc.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+    @Test
+    public void test1(){
+        log.debug("test1");
+        log.info("test1");
+        log.error("test1");
+    }
+}
